e52260fd045ae1084f50dd645a84fd3c2c1caec9
Ben Carpenter <carpe504@d.umn.edu>
2017-07-30 15:39:51 -0500

    Bot responses should not contain the trigger word...

--- bot.js
+++ bot.js
@@ -5,9 +5,11 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /swamp/;
+  //callback trigger probably shouldn't be in the response...
+      botRegex = /\/swamp/;
 
   if(request.text && botRegex.test(request.text)) {
+    console.log("Regex passed!");
     this.res.writeHead(200);
     postMessage();
     this.res.end();
