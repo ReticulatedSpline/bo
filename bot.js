49fdd2ea98c44ca71a1cbbe0b0a9f9da407799a5
Ben Carpenter <carpe504@d.umn.edu>
2017-08-01 23:44:02 -0500

    Laid groundwork for weather responses. Just need to fix async timing.

--- bot.js
+++ bot.js
@@ -1,16 +1,17 @@
 var HTTPS = require('https');
+var yql = require('yqlp');
 const fs = require('fs');
 
 var botID = process.env.BOT_ID;
 var sourceLink = "https://github.com/ReticulatedSpline/Groupme-Chatbot";
-var e= "Trigger detected: ";
+var e = "Trigger detected: ";
 
 function parseResponse() {
   var request = JSON.parse(this.req.chunks[0]),
-  //callback trigger probably shouldn't be in the response...
-      botRegex = /\@Bo/;
+    //callback trigger probably shouldn't be in the response...
+    botRegex = /\@Bo/;
 
-  if(request.text &&
+  if (request.text &&
     botRegex.test(request.text) &&
     request.sender_type != "bot") {
     var botResponse = "";
@@ -26,23 +27,25 @@ function parseResponse() {
       case (/flip.*coin/.test(request.text)):
         botResponse = buildCoinFlip();
         break;
-      case (/pick.*number.*between/.test(request.text)):
-        botResponse = buildNumPick(request.text);
+      case (/.*number.*between/.test(request.text)):
+        botResponse = setTimeout(() => buildNumPick(request.text), 2000);
+        break;
+      case (/weather/.test(request.text)):
+        botResponse = buildWeather(request.text);
         break;
       default:
         console.log(e + "basecase");
         botResponse = "My responses are limited. You can see a list of valid" +
-                      " queries with `@Bo help`.";
+          " queries with `@Bo help`.";
     }
 
     this.res.writeHead(200);
     postMessage(botResponse);
     this.res.end();
-  } else {
-    console.log(e + "none");
+  } /*else {
     this.res.writeHead(200);
     this.res.end();
-  }
+  }*/
 }
 
 function buildAbout() {
@@ -54,7 +57,7 @@ function buildCoinFlip() {
   console.log(e + "coinflip");
   var result = Math.floor(Math.random() * 2);
   return result > 0 ? "The coin was heads!" :
-                      "The coin was tails.";
+    "The coin was tails.";
 }
 
 function buildNumPick(req) {
@@ -68,6 +71,37 @@ function buildNumPick(req) {
   return "I choose " + (Math.floor(Math.random() * max) + min) + "!";
 }
 
+function buildWeather(req) {
+  console.log(e + "weather");
+  //@Bo weather in duluth, mn
+  var city = req.match(/[a-z]+(?=\,)/);
+  var state = req.match(/(?:,\s)(.*)/);
+  if (!city || !state) {
+    console.log("Invalid arguments. Defaulting...")
+    city = "duluth";
+    state = "mn";
+  } else {
+    city = city[0];
+    state = state[0];
+  }
+
+  console.log("Querying " + city + ", " + state);
+
+  var query = 'select * from weather.forecast where ' +
+    'woeid in (select woeid from geo.places(1) ' +
+    'where text="' + city + ', ' + state + '")';
+  return yql.execp(query, function(err, data) {
+    if (err) {
+      resolve("Sorry, but the request failed.");
+    } else {
+      var location = data.query.results.channel.location;
+      var condition = data.query.results.channel.item.condition;
+      resolve('It is ' + condition.text + ' in ' + location.city + ', ' +
+        location.region + ' and ' + condition.temp + ' degrees.');
+    }
+  });
+}
+
 function postMessage(botResponse) {
   var botResponse, options, body, botReq;
 
@@ -78,25 +112,25 @@ function postMessage(botResponse) {
   };
 
   body = {
-    "bot_id" : botID,
-    "text" : botResponse
+    "bot_id": botID,
+    "text": botResponse
   };
 
   console.log('Sending: \'' + botResponse + '\' to ' + botID);
 
   botReq = HTTPS.request(options, function(res) {
-      if(res.statusCode == 202) {
-        console.log('Response code: ' + res.statusCode);
-      } else {
-        console.log('Response code: ' + res.statusCode);
-      }
+    if (res.statusCode == 202) {
+      console.log('Response code: ' + res.statusCode);
+    } else {
+      console.log('Response code: ' + res.statusCode);
+    }
   });
 
   botReq.on('error', function(err) {
-    console.log('Error posting message '  + JSON.stringify(err));
+    console.log('Error posting message ' + JSON.stringify(err));
   });
   botReq.on('timeout', function(err) {
-    console.log('Timeout posting message '  + JSON.stringify(err));
+    console.log('Timeout posting message ' + JSON.stringify(err));
   });
   botReq.end(JSON.stringify(body));
 }
