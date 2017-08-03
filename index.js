c1c892db583211f878f416618900553773b4be47
ReticulatedSpline <carpenter.ben@icloud.com>
2017-08-02 21:21:03 -0500

    Corrected username

--- index.js
+++ index.js
@@ -29,5 +29,5 @@ server.listen(port);
 
 function ping() {
   this.res.writeHead(200);
-  this.res.end("You've reached Bo, but I don't communicate on this website.");
+  this.res.end("You've reached Bo, but I don't communicate on this website. Sorry!");
 }
